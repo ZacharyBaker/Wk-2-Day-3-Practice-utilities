c522be6528a6c0b03274b997a5fdab8370c24174
Zach Baker <zach.r.baker1@gmail.com>
2015-10-19 20:29:22 -0600

    return index

--- src/underscore_tests.js
+++ src/underscore_tests.js
@@ -39,15 +39,33 @@ var _ = { };
   // Call iterator(value, key, collection) for each element of collection.
   // Accepts both arrays and objects.
   _.each = function(collection, iterator) {
-  };
+    if (Array.isArray(collection)){
+      collection.forEach(iterator);
+    } else {
+    for (var prop in collection){
+      iterator(collection[prop], prop, collection);
+    }
+  }
+ }
 
   // Returns the index at which value can be found in the array, or -1 if value
   // is not present in the array.
   _.indexOf = function(array, target){
+    for (var i = 0; i < array.length; i++){
+      if (array[i]===target){
+        return i;
+      } 
+      }
+   return -1;
   };
+// why the freak can't i use this |return array.indexOf(target);|
+
+
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, iterator) {
+    
+    
   };
 
   // Return all elements of an array that don't pass a truth test.
